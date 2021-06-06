0ec6ecc13903352440c7a7414742ba7244d3e57d
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-06 21:18:24 +0700

    handle dynamic data from controller

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,30 +2,43 @@
 
 @section('content')
     <div class="w-4/5 m-auto text-center">
-      <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl">Blog Posts</h1>
-      </div>
+        <div class="py-15 border-b border-gray-200">
+            <h1 class="text-6xl">Blog Posts</h1>
+        </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-      <div>
-        <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
-      </div>
-      <div>
-        <h2 class="text-gray-700 font-bold text-5xl pb-4">Learn how to write Laravel code</h2>
-        <span class="text-gray-500">
-          By <span class="font-bold italic text-gray-800">Code With Zhuhry</span>, 1 day ago
-        </span>
-
-        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, numquam nam ratione, 
-          ipsum possimus quia, delectus rem quibusdam esse doloremque enim est eius impedit ea provident? 
-          Repellendus totam neque molestiae?
-        </p>
-
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-          Keep Reading
-        </a>
-      </div>
+@if (Auth::check())
+    <div class="pt-15 w-4/5 m-auto">
+      <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+        Create post
+      </a>
     </div>
+@endif
+
+@foreach ($posts as $post)
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            </div>
+            <div>
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                    {{ $post->title }}
+                </h2>
+                <span class="text-gray-500">
+                    By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on
+                    {{ date('jS M Y', strtotime($post->updated_at)) }}
+                </span>
+
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ $post->description }}
+                </p>
+
+                <a href="/blog/{{ $post->slug }}"
+                    class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Keep Reading
+                </a>
+            </div>
+        </div>
+@endforeach
+
 @endsection
